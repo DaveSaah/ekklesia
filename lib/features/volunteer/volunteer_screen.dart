5a8791b839eb500d04717181dbed99954b899219
David Saah <davesaah@gmail.com>
2025-05-01 14:55:57 +0000

    chore: add feature skeletons

---
+++ lib/features/volunteer/volunteer_screen.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class VolunteerScreen extends StatefulWidget {
+  const VolunteerScreen({super.key});
+
+  @override
+  State<VolunteerScreen> createState() => _VolunteerScreenState();
+}
+
+class _VolunteerScreenState extends State<VolunteerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Text("Volunteers"));
+  }
+}
