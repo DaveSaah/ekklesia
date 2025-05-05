4dcb1c7cfa2feda37aaeca13af07afec42b2cb41
David Saah <davesaah@gmail.com>
2025-05-05 21:49:08 +0000

    feat: add user service

---
+++ lib/services/user_service.dart
@@ -0,0 +1,43 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class UserService {
+  final _client = Supabase.instance.client;
+
+  Future<String?> getUuid() async {
+    return _client.auth.currentUser?.id;
+  }
+
+  Future<String> getDisplayName() async {
+    // load from cache
+    final displayName = await _getNameFromCache();
+    if (displayName != null) {
+      return displayName;
+    }
+
+    final userId = getUuid();
+
+    final response =
+        await _client
+            .from('profiles')
+            .select('display_name')
+            .eq('id', userId)
+            .single();
+
+    _cacheName(response['display_name']);
+
+    return response['display_name'];
+  }
+
+  // Cache display name
+  Future<void> _cacheName(String name) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('display_name', name);
+  }
+}
+
+// Fetch the cached name from shared preferences
+Future<String?> _getNameFromCache() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString('display_name');
+}
