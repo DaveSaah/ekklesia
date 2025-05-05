ec8355bafb1938298116a93bcbfa0de14639a94d
David Saah <davesaah@gmail.com>
2025-05-05 16:59:52 +0000

    feat: app color definitions

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.deepOrange;
+  static const Color secondary = Colors.deepOrange;
+  static const Color textPrimary = Colors.black87;
+  static const Color textSecondary = Colors.grey;
+  static const Color buttonPrimary = Colors.white;
+  static const Color error = Colors.redAccent;
+  static const Color divider = Colors.grey;
+  static const Color accentColor = Colors.white;
+}
